63a3c0a5ef0ba362375be030939322ed17be7123
jurgilijus <jurgissubacius@gmail.com>
2022-09-12 18:00:57 +0300

    Laba diena

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const vardas = 6;
+
+function Greetings() {
+  return <h4>Laba diena esu Jurgis ir tai mano pirmasis komponentas</h4>;
+}
+
+root.render(<Greetings />);
