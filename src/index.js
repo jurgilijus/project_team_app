3cadaf4ddf83453f4bdc1fa3daac1d1a3fefa33b
jurgilijus <jurgissubacius@gmail.com>
2022-09-13 13:00:42 +0300

    Book list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const vardas = 6;
 
-function Greetings() {
-  return <h4>Laba diena esu Jurgis ir tai mano pirmasis komponentas</h4>;
+function BookList() {
+  return (
+    <div>
+      <Book />
+    </div>
+  );
 }
 
-root.render(<Greetings />);
+const Book = () => {
+  return (
+    <div>
+      <Image />
+      <Title />
+      <Author />
+    </div>
+  );
+};
+
+const Image = () => {
+  return <img src="./images/rose.png" alt="" className="image" />;
+};
+
+const Title = () => {
+  return <h2>I love you to the moon an back</h2>;
+};
+const Author = () => {
+  return <h4>Amelia Hepworth</h4>;
+};
+
+root.render(<BookList />);
