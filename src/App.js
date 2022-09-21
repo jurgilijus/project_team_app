dccfe6d8447bd12bb8df667107c44e11751389f4
jurgilijus <jurgissubacius@gmail.com>
2022-09-21 16:05:29 +0300

    manu done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Sidebar from "./sidebar/sidebar";
-import BookShop from "./bookshop/bookshop";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./Components/Sidebar.js";
+import Home from "./Pages/Home";
+import Bookshop from "./Pages/Bookshop/bookshop";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Sidebar />
-      {/* <BookShop /> */}
-    </div>
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/bookshop" element={<Bookshop />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,41 +1,33 @@
-import React, { Children } from "react";
-import { AiFillHome, FaShoppingCart, FaBars } from "react-icons/fa";
+import React from "react";
+import { FaHome, FaShoppingCart } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-
-function Sidebar({}) {
-  const menuItem = () => [
-    {
-      path: "/",
-      name: "Home",
-      icon: <AiFillHome />,
-    },
-    {
-      path: "/bookshop",
-      name: "Bookshop",
-      icon: <FaShoppingCart />,
-    },
-  ];
+const menuItem = [
+  {
+    path: "/home",
+    name: "Home",
+    icon: <FaHome />,
+  },
+  {
+    path: "/bookshop",
+    name: "Bookshop",
+    icon: <FaShoppingCart />,
+  },
+];
+function Sidebar({ children }) {
   return (
     <div className="conteiner">
       <div className="sidebar">
         <div className="top_section">
-          <h1 className="logo"></h1>
-          <div className="bars">
-            <FaBars />
-          </div>
+          <h1 className="logo">Menu</h1>
         </div>
         {menuItem.map((item, index) => (
-          <NavLink
-            to={item.path}
-            key={index}
-            className="link"
-            activeclassName="active"
-          >
+          <NavLink to={item.path} key={index} className="link">
             <div className="icon">{item.icon}</div>
             <div className="link_text">{item.name}</div>
           </NavLink>
         ))}
       </div>
+      <main>{children}</main>
     </div>
   );
 }
